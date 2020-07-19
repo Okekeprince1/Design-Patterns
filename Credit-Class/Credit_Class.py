d92db90b6bdc7762d8613c1e2b0529ad0315a6d7
Okekeprince1 <okeke.p.ebube@gmail.com>
2020-07-19 13:32:10 +0100

    Initialized constructor

--- Credit-Class/Credit_Class.py
+++ Credit-Class/Credit_Class.py
@@ -1,1 +1,11 @@
-class Credit_Card:
+#Initalized CreditCard class
+class CreditCard:
+    """A customer credit card."""
+    def __init__(self, customer, bank, account, limit)
+    #The initial balance is zero
+
+    self._balance = 0
+    self._customer = customer
+    sel._bank = bank
+    self._account = account
+    self._limit = limit
