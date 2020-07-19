55bdeb3888ec9d559f536dd48c1ff508162d38dc
Okekeprince1 <okeke.p.ebube@gmail.com>
2020-07-19 13:49:18 +0100

    Defined methods

--- Credit-Class/Credit_Class.py
+++ Credit-Class/Credit_Class.py
@@ -1,4 +1,4 @@
-#Initalized CreditCard class
+#Created CreditCard class
 class CreditCard:
     """A customer credit card."""
     def __init__(self, customer, bank, account, limit)
@@ -9,3 +9,42 @@ class CreditCard:
     sel._bank = bank
     self._account = account
     self._limit = limit
+
+    def get_customer(self):
+        #return name of customer
+        return self._customer
+    
+    def get_bank(self):
+        #return bank name
+        return self._bank
+    
+    def get_account(self):
+        """Return the Account number 
+        (Stored as a string)"""
+        return self._account
+
+    def get_limit(self):
+        """Returns credit card limit"""
+        return self._limit
+    
+    def get_balance(self):
+        """Returns the credit card balance"""
+        return self._balance
+
+
+    #Functions that manipulate constructors
+
+    def charge(self, price):
+        """Charge given price to the card, assuming sufficient credit limit.
+        
+        Return True if charge was processed; False if charge was denied"""
+        if price + self._balance > self._limit
+            return False
+        else:
+            self.balance += price
+            return True
+        
+    def make_payment(self, amount):
+        """Make payment that reduces balance"""
+        self.balance -= amount
+
